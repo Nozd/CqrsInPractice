00828b7f0f45560a7c6a430ec3850652db25b96e
nozdUp <nozdup@gmail.com>
2023-01-28 17:14:50 +0300

    Move CourseRepository to the separate class

---
+++ Before/src/Logic/Students/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+using Logic.Utils;
+
+namespace Logic.Students
+{
+    public sealed class CourseRepository
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public CourseRepository(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Course GetByName(string name)
+        {
+            return _unitOfWork.Query<Course>()
+                .SingleOrDefault(x => x.Name == name);
+        }
+    }
+}
--- Before/src/Logic/Students/StudentRepository.cs
+++ Before/src/Logic/Students/StudentRepository.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-using Logic.Utils;
+﻿using Logic.Utils;
 
 namespace Logic.Students
 {
@@ -26,23 +24,6 @@ namespace Logic.Students
         public void Delete(Student student)
         {
             _unitOfWork.Delete(student);
-        }
-    }
-
-
-    public sealed class CourseRepository
-    {
-        private readonly UnitOfWork _unitOfWork;
-
-        public CourseRepository(UnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-
-        public Course GetByName(string name)
-        {
-            return _unitOfWork.Query<Course>()
-                .SingleOrDefault(x => x.Name == name);
         }
     }
 }
